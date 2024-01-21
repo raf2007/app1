f2b864a5fa5127d3e7f0eae9c7cc5ce0d6abf6f2
Raffi <mehrabyanraffi@mail.ru>
2024-01-21 23:06:04 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/carapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.carapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button registerButton = findViewById(R.id.buttonRegister);
+        Button loginButton = findViewById(R.id.buttonLogin);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
